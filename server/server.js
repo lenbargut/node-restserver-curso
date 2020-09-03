a4092a254c71275ac6fee100b47cc56286cd1d82
Lenis <lebebagu@gmail.com>
2020-09-03 13:47:00 -0600

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config.js');
 
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express()
 const bodyParser = require('body-parser');
 
@@ -10,39 +12,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-})
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-
-        });
-
-    } else {
-        res.json({ persona: body });
-    }
+app.use(require('./routes/usuario.js'));
 
 
-});
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
 
-app.put('/usuario/:id', function(req, res) {
+        if (err) throw err;
 
-    let id = req.params.id;
-    res.json({ id });
-});
+        console.log('Base de datos ONLINE');
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-})
+    });
 
 
 app.listen(process.env.PORT, () => {
