c5b0e4c0fb712fbf30827590644ba8ae04e91a05
Lenis <lebebagu@gmail.com>
2020-09-10 16:48:23 -0600

    Login normal seccion 10

--- server/server.js
+++ server/server.js
@@ -7,12 +7,18 @@ const app = express()
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
+
 app.use(bodyParser.json())
 
-app.use(require('./routes/usuario.js'));
+
+// Configuracion global de rutas
+
+app.use(require('./routes/index.js'));
+
 
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true },
